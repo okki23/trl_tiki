306049e0fcfdd95baaf6c541af617994d4f4fe01
okki23 <okkisetyawan@gmail.com>
2021-04-23 08:20:24 +0700

    sync first

---
+++ application/modules/Texp/models/MTexp.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * author 	: Okki Setyawan
+ * email 	: okkisetyawan@gmail.com
+ * github	: github.com/okki23
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MTexp extends CI_Controller {
+ 
+	public function index()
+	{
+		echo 'welcome';
+	}
+}
