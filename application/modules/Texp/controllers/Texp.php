f5b13efe8084f8f15f3dfb99a00d63f3389c67be
okki23 <okkisetyawan@gmail.com>
2021-05-02 07:34:50 +0700

    connote_history

--- application/modules/Texp/controllers/Texp.php
+++ application/modules/Texp/controllers/Texp.php
@@ -57,8 +57,59 @@ class Texp extends CI_Controller {
 						curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arraybody));           
 						curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 						$result     = curl_exec ($ch);
-						$statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-						// var_dump($result);
+						$statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE); 
+						echo $result;
+	}
+
+	public function connote_history(){
+		$awb = $this->input->post('awb'); 
+		$token = $this->input->post('token'); 
+		  
+		$endpoint = 'http://apis.mytiki.net:8321/connote/mpds/history';  
+		$headers  = [ 
+	        'Content-Type: application/json',
+			'x-access-token:'.$token
+        ];
+ 
+		$arraybody = array('cnno'=>$awb);  
+						$ch = curl_init();
+						curl_setopt($ch, CURLOPT_URL,$endpoint);
+						curl_setopt($ch, CURLOPT_POST, 1);
+						curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+						curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arraybody));           
+						curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+						$result     = curl_exec ($ch); 
+						$statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE); 
+						 
+		//explode
+		$my_var = json_decode($result, true);
+
+		unset($my_var["status"],$my_var["msg"]);
+
+		$json = json_encode($my_var);
+		echo $json;
+		 
+	}
+
+	public function connote_status(){
+		$awb = $this->input->post('awb'); 
+		$token = $this->input->post('token'); 
+		  
+		$endpoint = 'http://apis.mytiki.net:8321/connote/statuscode';  
+		$headers  = [ 
+			'x-access-token:'.$token,
+            'Content-Type: application/json'
+        ];
+ 
+		$arraybody = array('cnno'=>$awb);  
+						$ch = curl_init();
+						curl_setopt($ch, CURLOPT_URL,$endpoint);
+						curl_setopt($ch, CURLOPT_POST, 1);
+						curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+						curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arraybody));           
+						curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+						$result     = curl_exec ($ch);
+						$statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE); 
 						echo $result;
 	}
  
