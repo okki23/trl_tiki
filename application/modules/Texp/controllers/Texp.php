c57b6a20a10c9d62d6407f50b989a9b6d6dfbc26
okki23 <okkisetyawan@gmail.com>
2021-05-02 22:02:27 +0700

    push manifest

--- application/modules/Texp/controllers/Texp.php
+++ application/modules/Texp/controllers/Texp.php
@@ -99,15 +99,52 @@ class Texp extends CI_Controller {
 		$headers  = [ 
 			'x-access-token:'.$token,
             'Content-Type: application/json'
-        ];
- 
-		// $arraybody = array('cnno'=>$awb);  
+        ]; 
 						$ch = curl_init();
 						curl_setopt($ch, CURLOPT_URL,$endpoint);
 						curl_setopt($ch, CURLOPT_POST, 1);
-						curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-						// curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arraybody));           
+						curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);          
 						curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+						$result     = curl_exec ($ch);
+						$statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE); 
+						echo $result;
+	}
+
+	public function push_manifest(){
+		 
+		$token = $this->input->post('token'); 
+		 
+ 
+		$endpoint = 'http://apis.mytiki.net:8321/mde/manifestorder';  
+		$headers  = [ 
+			'x-access-token:'.$token,
+            'Content-Type: application/json'
+        ]; 
+
+		$arraybody = array('accnum'=>$this->input->post('accnum'),
+						   'paket_awb'=>$this->input->post('paket_awb'),
+						   'paket_id'=>$this->input->post('paket_id'),
+						   'paket_content'=>$this->input->post('paket_content'),
+						   'paket_value'=>$this->input->post('paket_value'),
+						   'paket_cod'=>$this->input->post('paket_cod'),
+						   'paket_service'=>$this->input->post('paket_service'),
+						   'consignor_name'=>$this->input->post('consignor_name'),
+						   'consignor_company'=>$this->input->post('consignor_company'),
+						   'consignor_address1'=>$this->input->post('consignor_address1'),
+						   'consignor_address2'=>$this->input->post('consignor_address2'),
+						   'consignor_zipcode'=>$this->input->post('consignor_zipcode'),
+						   'consignor_phone'=>$this->input->post('consignor_phone'),
+						   'consignee_name'=>$this->input->post('consignee_name'),
+						   'consignee_company'=>$this->input->post('consignee_company'),
+						   'consignee_address1'=>$this->input->post('consignee_address1'),
+						   'consignee_address2'=>$this->input->post('consignee_address2'),
+							);
+						$ch = curl_init();
+						curl_setopt($ch, CURLOPT_URL,$endpoint);
+						curl_setopt($ch, CURLOPT_POST, 1);
+						curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);          
+						curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+						curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arraybody));    
 						$result     = curl_exec ($ch);
 						$statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE); 
 						echo $result;
